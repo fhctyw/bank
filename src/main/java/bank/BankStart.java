2ddbb21a4d229a25343cccba67cfa6b001bb7382
Arsen <lisnychukarsenw@gmail.com>
2022-11-05 15:25:57 +0200

    Create empty spring starter for bank

---
+++ src/main/java/bank/BankStart.java
@@ -0,0 +1,11 @@
+package bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStart {
+    public static void main(final String[] args) {
+        SpringApplication.run(BankStart.class, args);
+    }
+}
