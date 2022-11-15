fc81836c54ee2b5f3171eced87ce3fd1142aac17
RIO489 <oleksander.hamaiunov.it.2021@lpnu.ua>
2022-11-15 10:11:47 +0200

    transaction quick fixes +  /history(done?)

--- src/main/java/bank/util/JacksonUtil.java
+++ src/main/java/bank/util/JacksonUtil.java
@@ -20,10 +20,10 @@ public class JacksonUtil {
 
     public static <T> T deserialize(final String source, final TypeReference<T> clazz) {
         try {
-            ObjectMapper mapper = new ObjectMapper();//
-            mapper.registerModule(new JavaTimeModule());//
-            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);//
-            return mapper.readValue(source, clazz);
+//            ObjectMapper mapper = new ObjectMapper();//
+//            mapper.registerModule(new JavaTimeModule());//
+//            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);//
+            return objectMapper.readValue(source, clazz);
         } catch (final JsonProcessingException e) {
             e.printStackTrace();
             return null;
